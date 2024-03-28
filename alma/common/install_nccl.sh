76795b78caf7e560de79a4656e8fb1c710892d93
Michael Weiser <michael.weiser@eviden.com>
2024-03-28 14:03:04 +0100

    Tune NCCL build paralellisation

--- alma/common/install_nccl.sh
+++ alma/common/install_nccl.sh
@@ -22,7 +22,7 @@ wget ${NCCL_DOWNLOAD_URL}
 tar -xf ${TARBALL}
 
 pushd nccl-${NCCL_VERSION}
-make -j src.build
+make -j$(nproc) src.build
 make pkg.redhat.build
 rpm -i ./build/pkg/rpm/x86_64/libnccl-${NCCL_VERSION}+cuda${CUDA_DRIVER_VERSION}.x86_64.rpm
 rpm -i ./build/pkg/rpm/x86_64/libnccl-devel-${NCCL_VERSION}+cuda${CUDA_DRIVER_VERSION}.x86_64.rpm
@@ -37,7 +37,7 @@ pushd nccl-rdma-sharp-plugins
 git checkout ${NCCL_RDMA_SHARP_COMMIT}
 ./autogen.sh
 ./configure --prefix=/usr/local/nccl-rdma-sharp-plugins --with-cuda=/usr/local/cuda
-make
+make -j$(nproc)
 make install
 popd
 popd
@@ -47,7 +47,7 @@ source /etc/profile.d/modules.sh
 module load mpi/hpcx
 git clone https://github.com/NVIDIA/nccl-tests.git
 pushd nccl-tests
-make MPI=1 MPI_HOME=${HPCX_MPI_DIR} CUDA_HOME=/usr/local/cuda
+make -j$(nproc) MPI=1 MPI_HOME=${HPCX_MPI_DIR} CUDA_HOME=/usr/local/cuda
 popd
 mv nccl-tests /opt/.
 module unload mpi/hpcx
